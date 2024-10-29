e9465c45179a9f251ebec19031dad1d7c428d3b5
Zeu Capua <coding@zeu.dev>
2024-10-29 11:15:29 -0700

    remove iron-session, add cookie params

--- src/routes/oauth/callback/+server.ts
+++ src/routes/oauth/callback/+server.ts
@@ -1,12 +1,12 @@
 import { atclient } from "$lib/server/client";
-import type { RequestEvent } from "../../$types";
 import { error, redirect } from "@sveltejs/kit";
+import type { RequestEvent } from "../../$types";
 
 export async function GET({ request, cookies }: RequestEvent) {
   const params = new URLSearchParams(request.url.split("?")[1]);
   try {
     const { session } = await atclient.callback(params)
-    cookies.set("sid", session.did, { path: "/" });
+    cookies.set("sid", session.did, { path: "/", maxAge: 60 * 60, httpOnly: true, sameSite: "lax" });
   } catch (err) {
     console.log(err);
     error(500, { message: (err as Error).message });
