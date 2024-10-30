76de67d0c35a77eb8a56651f09b9c75c8e99b0bf
Zeu Capua <coding@zeu.dev>
2024-10-29 21:38:17 -0700

    update build command, refacor at client variable, init runScheduledPost endpoint

--- src/lib/server/client.ts
+++ src/lib/server/client.ts
@@ -1,5 +1,5 @@
 import { NodeOAuthClient } from "@atproto/oauth-client-node";
-import { db as database } from "./db";
+import { db } from "./db";
 import { SessionStore, StateStore } from "./storage";
 import { dev } from "$app/environment";
 
@@ -7,22 +7,18 @@ const publicUrl = "https://myb.zeu.dev";
 const url = dev ? "http://[::1]:5173" : publicUrl; // since I'm using ipv6, use ::1 instead 127.0.0.1
 const enc = encodeURIComponent;
 
-const createClient = async (db: typeof database) => {
-  return new NodeOAuthClient({
-    stateStore: new StateStore(db),
-    sessionStore: new SessionStore(db),
-    clientMetadata: {
-      client_name: "myb",
-      client_id: !dev ? `${publicUrl}/client-metadata.json` : `http://localhost?redirect_uri=${enc(`${url}/oauth/callback`)}&scope=${enc('atproto transition:generic')}`,
-      client_uri: url,
-      redirect_uris: [`${url}/oauth/callback`],
-      scope: "atproto transition:generic",
-      grant_types: ["authorization_code", "refresh_token"],
-      application_type: "web",
-      token_endpoint_auth_method: "none",
-      dpop_bound_access_tokens: true
-    }
-  })
-};
-
-export const atclient = await createClient(database);
+export const atclient = new NodeOAuthClient({
+  stateStore: new StateStore(db),
+  sessionStore: new SessionStore(db),
+  clientMetadata: {
+    client_name: "myb",
+    client_id: !dev ? `${publicUrl}/client-metadata.json` : `http://localhost?redirect_uri=${enc(`${url}/oauth/callback`)}&scope=${enc('atproto transition:generic')}`,
+    client_uri: url,
+    redirect_uris: [`${url}/oauth/callback`],
+    scope: "atproto transition:generic",
+    grant_types: ["authorization_code", "refresh_token"],
+    application_type: "web",
+    token_endpoint_auth_method: "none",
+    dpop_bound_access_tokens: true
+  }
+});
---
+++ src/routes/runScheduledPost/+server.ts
@@ -0,0 +1,5 @@
+import type { RequestEvent } from "../$types";
+
+export async function POST({ request, cookies }: RequestEvent) {
+  
+}
