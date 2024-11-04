d786c4ee3653d12e9dfedf0de93be2c4da42b856
Zeu Capua <coding@zeu.dev>
2024-11-04 12:33:23 -0800

    render posts with facets, toggle reposts on profile feed, more profile details

--- src/routes/p/[handle]/+page.server.ts
+++ src/routes/p/[handle]/+page.server.ts
@@ -1,6 +1,7 @@
+import { marked } from "marked";
 import { error } from "@sveltejs/kit";
 import { HandleResolver } from "@atproto/identity";
-import { Agent, AtpBaseClient } from "@atproto/api";
+import { Agent, AtpBaseClient, RichText } from "@atproto/api";
 import type { PageServerLoadEvent } from "./$types";
 
 export async function load({ locals, params }: PageServerLoadEvent) {
@@ -22,6 +23,36 @@ export async function load({ locals, params }: PageServerLoadEvent) {
     const profile = await agent.app.bsky.actor.getProfile({ actor: did });
     const feed = await agent.app.bsky.feed.getAuthorFeed({ actor: did });
 
+    for (const post of feed.data.feed) {
+      // @ts-ignore
+      post.html = await renderTextToMarkdownToHTML(post.post.record.text, agent);
+    }
+
     return { profile: profile.data, feed: JSON.stringify(feed.data.feed) } 
   }
+}
+
+async function renderTextToMarkdownToHTML(text: string, agent: Agent | AtpBaseClient) {
+  const rt = new RichText({ text });
+  await rt.detectFacets(agent);
+  let markdown = "";
+  for (const segment of rt.segments()) {
+    if (segment.isLink()) {
+      markdown += `[${segment.text}](${segment.link?.uri})`
+    } else if (segment.isMention()) {
+      let profile;
+      if (agent instanceof Agent) {
+        profile = await agent.getProfile({ actor: segment.mention?.did || "" });
+      }
+      else {
+        profile = await agent.app.bsky.actor.getProfile({ actor: segment.mention?.did || "" });
+      }
+      markdown += `[${segment.text}](https://myb.zeu.dev/p/${profile.data.handle})`
+    } else {
+      markdown += segment.text
+    }
+  }
+
+  const html = await marked.parse(markdown);
+  return html;
 }
--- src/routes/p/[handle]/+page.svelte
+++ src/routes/p/[handle]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Icon from '@iconify/svelte';
   import type { FeedViewPost } from '@atproto/api/dist/client/types/app/bsky/feed/defs';
   import type { ProfileViewDetailed } from '@atproto/api/dist/client/types/app/bsky/actor/defs';
   
@@ -10,24 +11,57 @@
   };
 
   let { data }: Props = $props();
-  const feed = JSON.parse(data.feed) as FeedViewPost[];
+  const feed = JSON.parse(data.feed) as FeedViewPost & { html: string }[];
+
+  let showReposts = $state(true);
 </script>
 
 {#snippet postDisplay(data: FeedViewPost)}
-  <article class="flex flex-col gap-4 border px-4 py-2 rounded">
-    <h1>{data.post.author.handle}</h1>
-    <p>
-    {
-      // @ts-ignore
-      data.post.record.text
-    }
+  <article class={`flex flex-col gap-4 border px-4 py-2 rounded ${data.reason && "border-dashed"}`}>
+    <div class="flex items-center justify-between w-full">
+      <h1>{data.post.author.handle} </h1>
+      <div class="flex gap-2">
+        {#if data.reason}
+          <Icon icon="bx:repost" />
+        {/if}
+        {#if data.reply}
+          <Icon icon="ic:baseline-reply" />
+        {/if}
+      </div>
+    </div>
+    <p class="prose prose-invert">
+      {@html 
+        // @ts-ignore
+        data.html 
+      }
     </p>
   </article>
 {/snippet}
 
-<img src={data.profile.avatar} alt={`${data.profile.handle} profile picture`} class="size-16" />
-<p>{data.profile.handle}</p>
+<section class="flex gap-4 items-center">
+  <img src={data.profile.avatar} alt={`${data.profile.handle} profile picture`} class="size-16 rounded" />
+  <div class="flex flex-col">
+    <h1 class="text-lg font-bold">{data.profile.displayName}</h1>
+    <h2 class="font-medium">@{data.profile.handle}</h2>
+    <div class="flex gap-4 font-thin">
+      <p>{data.profile.followersCount} Followers</p>
+      <p>{data.profile.followsCount} Following</p>
+    </div>
+  </div>
+</section>
 
-{#each feed as post}
-  {@render postDisplay(post)}
-{/each}
+<menu class="flex gap-4">
+  <label class="flex gap-2">
+    <input type="checkbox" bind:checked={showReposts} />
+    Reposts
+  </label>
+</menu>
+
+
+<ol class="flex flex-col gap-4">
+  {#each feed as post}
+    {#if !(post.reason && !showReposts)}
+      <li>{@render postDisplay(post)}</li>
+    {/if}
+  {/each}
+</ol>
