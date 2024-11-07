b03e5ccf164c5f12b640a1cd239933e29b637796
Zeu Capua <coding@zeu.dev>
2024-11-06 16:23:52 -0800

    implement posthog + nav/menu captures, show discover/follow feed on home page, init console, remove vercel analytics

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,16 @@
+import posthog from "posthog-js";
+import { browser } from "$app/environment";
 import type { LayoutServerLoadEvent } from "./$types";
+import { POSTHOG_API_KEY } from "$env/static/private";
 
 export async function load({ locals }: LayoutServerLoadEvent) {
+  if (browser) {
+    posthog.init(POSTHOG_API_KEY, {
+      api_host: "https://us.i.posthog.com",
+        person_profiles: "always"
+    });
+  }
+
   const user = locals.user;
   return { user }
 }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
   import '../app.css';
+  import posthog from 'posthog-js';
   import Icon from "@iconify/svelte";
-  import { dev } from '$app/environment';
-    import { goto } from '$app/navigation';
-  import { inject } from '@vercel/analytics';
+  import { goto } from '$app/navigation';
   import toast, { Toaster } from 'svelte-french-toast';
   import type { ProfileViewDetailed } from '@atproto/api/dist/client/types/app/bsky/actor/defs';
 
-  inject({ mode: dev ? 'development' : 'production' });
-
 	let { data, children } = $props();
   const user = data.user as ProfileViewDetailed;
+
+  let handleInput = $state("");
 
   function toastComingSoon() {
     toast("Coming soon", {
@@ -38,12 +37,26 @@
   <Toaster />
   <main class="flex flex-col gap-4 p-6 pb-16">
     <nav class="flex gap-4 justify-between items-center">
-      <a href="/" class="font-bold text-xl flex gap-2">
+      <a 
+        href="/" 
+        onclick={() => {
+          posthog.capture("clicked: top nav", { button: "Homepage logo" });
+        }}
+        class="font-bold text-xl flex gap-2"
+      >
         <Icon icon="game-icons:butterfly-warning" class="size-8" />
         myb
       </a>
       <div class="flex gap-4 items-center">
-        <a href="/search" class="underline underline-offset-4">Search</a>
+        <a 
+          href="/search" 
+          onclick={() => {
+            posthog.capture("clicked: top nav", { button: "Search link" });
+          }}
+          class="underline underline-offset-4"
+        >
+          Search
+        </a>
       </div>
     </nav>
 
@@ -52,24 +65,60 @@
 
   <menu class="fixed bg-slate-800 bottom-0 inset-x-0 flex w-full h-fit px-4 py-2 border-t justify-between">
     <div class="flex gap-4">
-      <button onclick={() => { !user ? toastError("Must be logged in to post") : goto("/console") }}>
+      <button onclick={() => { 
+        posthog.capture("clicked: bottom menu", { button: "Create Post" });
+        if (!user) { toastError("Must be logged in to post"); } 
+        else { goto("/console"); }
+      }}>
         <Icon icon="hugeicons:quill-write-02" class="size-8" />
       </button>
-      <button onclick={toastComingSoon}>
+      <button onclick={() => {
+        posthog.capture("clicked: bottom menu", { button: "Bookmarks" });
+        toastComingSoon();
+      }}>
         <Icon icon="hugeicons:all-bookmark" class="size-8" />
       </button>
     </div>
     {#if user}
       <form action="/?/logout" method="POST" class="flex gap-4">
-        <a href={`/p/${user.handle}`} data-sveltekit-reload>
+        <a 
+          href={`/p/${user.handle}`} 
+          onclick={() => {
+            posthog.capture("clicked: bottom menu", { button: "User profile" });
+          }}
+          data-sveltekit-reload
+        >
           <img src={user.avatar} alt={`${user.handle} profile picture`} class="size-10 rounded" />
         </a>
-        <button type="submit" class="border rounded px-4 py-2">Logout</button>
+        <button 
+          type="submit" 
+          onclick={() => {
+            posthog.capture("clicked: bottom menu", { button: "Logout" });
+          }}
+          class="border rounded px-4 py-2"
+        >
+          Logout
+        </button>
       </form>
     {:else}
       <form action="/?/login" method="POST" class="flex gap-2"> 
-        <input type="text" name="handle" placeholder="zeu.dev" class="border rounded px-4 py-2 bg-transparent max-w-32" />
-        <button type="submit" class="border rounded px-4 py-2">Login</button>
+        <input 
+          type="text" 
+          name="handle" 
+          placeholder="zeu.dev" 
+          bind:value={handleInput}
+          class="border rounded px-4 py-2 bg-transparent max-w-32" 
+        />
+        <button 
+          type="submit" 
+          onclick={() => {
+            posthog.capture("clicked: bottom menu", { button: "Login" });
+          }}
+          disabled={!handleInput}
+          class="border rounded px-4 py-2"
+        >
+          Login
+        </button>
       </form>
     {/if}
   </menu>
