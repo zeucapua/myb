3a6fe3642c9003de0f5a9f4eecc048b4af4dd65f
Zeu Capua <coding@zeu.dev>
2024-12-17 19:19:13 -0800

    remove autoplay for HlsPlayerEmbed

--- src/lib/components/HlsPlayerEmbed.svelte
+++ src/lib/components/HlsPlayerEmbed.svelte
@@ -5,6 +5,6 @@
   let { embed }: { embed: AppBskyEmbedVideo.View } = $props();
 </script>
 
-<media-player title={embed.alt} src={embed.playlist} controls autoPlay muted>
+<media-player title={embed.alt} src={embed.playlist} controls>
   <media-provider></media-provider>
 </media-player>
